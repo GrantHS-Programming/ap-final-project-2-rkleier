9824e3b22e875baab579cd15e2d916663227e0dc
rkleier6720 <rkleier6720@student.pps.net>
2023-05-12 09:48:21 -0700

    My head hurts

--- FinalProject.java
+++ FinalProject.java
@@ -2,40 +2,53 @@ import javax.swing.*;
 import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.util.ArrayList;
+
 public class FinalProject {
 
     public static void main(String[] args) {
-        game();
+
+
+
+
+
     }
-    public static void game(){
-        int str;
-        int spd;
-        int durability;
-
-
-
-
+    String[] races = new String[]{"Paragon", "Angel", "Dragon-Born", "War-Forged", "Elf", "Orc", "Human"};
+    String[] raceMessages = new String[]{"You are the pinnacle of all beings. A paragon.\n +3 to all stats", "You're an Angel. Have fun with the wings and getting hit on. \n +2 to all stats", }
+    int str;
+    int spd;
+    int durability;
+    public String raceDecider(){
+        String race;
         int raceNum = (int)(101 * Math.random());
-        if (raceNum == 1){//1%
-            System.out.println("You are the pinnacle of all beings. A paragon.");
+        if (raceNum == 1){//1% paragon
+            race = races[0];
+            return race;
         }
-        else if (raceNum < 5) {//3%
-            System.out.println("");
+        else if (raceNum < 5) {//3% angel
+            System.out.println();
+            race = races[1];
+            return race;
         }
         else if (raceNum < 10) {//5%
-
+            race = races[2];
+            return race;
         }
         else if (raceNum < 25) {//15%
-
+            race = races[3];
+            return race;
         }
-        else if (raceNum < 50) {//3%
-
+        else if (raceNum < 50) {//25%
+            race = races[4];
+            return race;
         }
-        else if (raceNum < 75) {//3%
-
+        else if (raceNum < 75) {//25%
+            race = races[5];
+            return race;
         }
-        else if (raceNum < 100) {//3%
-
+        else if (raceNum <= 100) {//26%
+            race = races[6];
+            return race;
         }
         System.out.println(raceNum);
     }
