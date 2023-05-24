7353048e1b52e2a58dbab05d8e1cd9263e6906d9
rkleier6720 <rkleier6720@student.pps.net>
2023-05-24 10:00:38 -0700

    getters and setters I had to look up because of how stupid the random races were

--- FinalProject.java
+++ FinalProject.java
@@ -7,50 +7,68 @@ import java.util.ArrayList;
 public class FinalProject {
 
     public static void main(String[] args) {
-
-
+        Player p1 = new Player(raceDecider(), races);
 
 
 
     }
-    String[] races = new String[]{"Paragon", "Angel", "Dragon-Born", "War-Forged", "Elf", "Orc", "Human"};
-    String[] raceMessages = new String[]{"You are the pinnacle of all beings. A paragon.\n +3 to all stats", "You're an Angel. Have fun with the wings and getting hit on. \n +2 to all stats",
-            "You are a Dragon-Born ,slightly harder to kill and easier to kill others. \n +1 to str \n +1 to spd \n +3 to dur",
-            "You are War-Forged, tankier and scarier but slow as hell. \n +2 to str \n -1 to spd \n +3 to dur", };
+    static String[] races = new String[]{"Paragon", "Angel", "Dragon-Born", "War-Forged", "Elf", "Orc", "Human"};
+    static String[] raceMessages = new String[]{"You are the pinnacle of all beings. A paragon.\n+3 to all stats", "You're an Angel. Have fun with the wings and getting hit on. \n+2 to all stats",
+            "You are a Dragon-Born ,slightly harder to kill and easier to kill others. \n+1 to str \n+1 to spd \n+3 to dur",
+            "You are War-Forged, tankier and scarier but slow as hell. \n+1 to str \n+2 to dur", "You are an Elf, a little faster than normal and a lot more elitest. \n+1 to speed",
+            "You are an Orc, a little stronger than normal but probably didn't arrive at the start of this dungeon on an academic scholarship. \n+1 to str",
+            "You are a human, boring but you're harder to kill due to the indomitable human spirit. \n+1 to dur"};
 
-    int health = 10;
-    int damage = 3;
-    int spd = 5;
-    public String raceDecider(){
+    static int health = 10;
+    static int damage = 3;
+    static int spd = 5;
+    public static Player turnStart(Player dude){
+
+
+
+
+
+
+
+        return dude;
+    }
+
+    public static String raceDecider(){
         String race;
         int raceNum = (int)(101 * Math.random());
         System.out.println(raceNum);
         if (raceNum == 1){//1% paragon
+            System.out.println(raceMessages[0]);
             race = races[0];
             return race;
         }
         else if (raceNum < 5) {//3% angel
-            System.out.println();
+            System.out.println(raceMessages[1]);
             race = races[1];
             return race;
         }
         else if (raceNum < 10) {//5%
+            System.out.println(raceMessages[2]);
             race = races[2];
             return race;
         }
         else if (raceNum < 25) {//15%
+            System.out.println(raceMessages[3]);
             race = races[3];
             return race;
         }
         else if (raceNum < 50) {//25%
+            System.out.println(raceMessages[4]);
             race = races[4];
             return race;
         }
         else if (raceNum < 75) {//25%
+            System.out.println(raceMessages[5]);
             race = races[5];
             return race;
         }
         else {//26%
+            System.out.println(raceMessages[6]);
             race = races[6];
             return race;
         }
--- Player.java
+++ Player.java
@@ -1,7 +1,35 @@
 public class Player {
     private int str = 1;
     private int spd = 1;
-    private int durability = 1;
+    private int dur = 1;
+
+    int health = 10;
+
+    int damage = 3;
+
+    int speed = 5;
+
+    public int getHealth() {
+        return health;
+    }
+
+    public int getDamage() {
+        return damage;
+    }
+    public int getSpeed() {
+        return speed;
+    }
+    public void setSpeed(int newSpeed) {
+        this.speed = speed;
+    }
+
+    public void setDamage(int newDamage) {
+        this.damage = damage;
+    }
+
+    public void setHealth(int newHealth) {
+        this.health = health;
+    }
 
     public Player (String race, String[] raceList){
         int intTracker = 0;
@@ -12,39 +40,32 @@ public class Player {
             }
         }
         if (intTracker ==0){
-            str =3;
-            spd =3;
-            durability =3;
+            str +=3;
+            spd +=3;
+            dur +=3;
         }
         else if (intTracker ==1){
-            str =2;
-            spd =2;
-            durability =2;
+            str +=2;
+            spd +=2;
+            dur +=2;
         }
         else if (intTracker ==2){
-            str =1;
-            spd =1;
-            durability =3;
+            str +=1;
+            spd +=1;
+            dur +=3;
         }
         else if (intTracker ==3){
-            str =3;
-            spd =3;
-            durability =3;
+            str +=1;
+            dur +=2;
         }
         else if (intTracker ==4){
-            str =3;
-            spd =3;
-            durability =3;
+            spd +=1;
         }
         else if (intTracker ==5){
-            str =3;
-            spd =3;
-            durability =3;
+            str +=1;
         }
         else {
-            str =3;
-            spd =3;
-            durability =3;
+            dur +=1;
         }
     }
 
